08f6bfff8935730db5af21cf114a29c72d565617
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2023-12-04 06:36:27 +0900

    refactor: テキストの変更

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 // script.js
 function changeText() {
-    var newText = "新しく動的に変更されたテキスト！";
+    var newText = "新しく動的に変更されたテキストなのだ！";
     document.getElementById("dynamicText").innerHTML = newText;
 }
